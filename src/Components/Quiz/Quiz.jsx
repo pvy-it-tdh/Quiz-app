018bc95e71f823766c0e64289b97043659f22649
pvy-it-tdh <phucvy107@gmail.com>
2024-04-06 21:17:07 +0700

    init file

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Quiz.css'
+const Quiz = () => {
+  return (
+    <div className='container'>
+        <h1>Quiz App</h1>
+    </div>
+  )
+}
+
+export default Quiz
