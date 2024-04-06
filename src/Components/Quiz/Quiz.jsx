02ef479d7b2af5d3474bf9f0e4c7b0ff68e10d17
pvy-it-tdh <phucvy107@gmail.com>
2024-04-06 23:32:57 +0700

    update

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -4,6 +4,16 @@ const Quiz = () => {
   return (
     <div className='container'>
         <h1>Quiz App</h1>
+        <hr />
+        <h2>Tôi là gì?</h2>
+        <ul>
+          <li>Phúc Vy</li>
+          <li>Phúc Vi</li>
+          <li>Phúc Duy</li>
+          <li>Phúc Vỹ</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 question</div>
     </div>
   )
 }
