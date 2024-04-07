41099bfa514d0020d9926969883be5380aed79ce
pvy-it-tdh <phucvy107@gmail.com>
2024-04-07 11:43:04 +0700

    up code

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Quiz.css'
+import {data} from '../../Assets/data'
 const Quiz = () => {
+  let [index,setIndex]=useState(0);
+  let [question,setQuestion]=useState(data[index]);
+
+  const checkAns =(e,ans)=>{
+    if(question.ans===ans)
+    {
+      e.target.classList.add("correct");
+    }
+    else
+    {
+      e.target.classList.add("")
+    }
+  }
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>Tôi là gì?</h2>
+        <h2>{index+1}.{question.question}</h2>
         <ul>
-          <li>Phúc Vy</li>
-          <li>Phúc Vi</li>
-          <li>Phúc Duy</li>
-          <li>Phúc Vỹ</li>
+          <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+          <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+          <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+          <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 question</div>
